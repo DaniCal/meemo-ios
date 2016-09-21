14264d7873d8d21b893d6ce1e7934fe22545979d
Daniel Lohse <daniel.lenerd.lohse@gmail.com>
2016-09-21 15:58:03 +0200

    ViewController - Basic sound played through AVAudioPlayer

--- Meemo/ViewController.swift
+++ Meemo/ViewController.swift
@@ -7,11 +7,18 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
+    
+    var sound: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        
+     
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +27,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func playButtonClick(_ sender: AnyObject) {
+//        var bombSoundEffect: AVAudioPlayer!
+
+        
+        
+        do {
+            let path = Bundle.main.path(forResource: "sound", ofType: "mp3")!
+            let url = URL(fileURLWithPath: path)
+            sound = try AVAudioPlayer(contentsOf: url)
+
+            sound.play()
+        } catch {
+            // couldn't load file :(
+        }
+    }
 
 }
 
