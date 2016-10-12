69c089a646b45c31b531236e8a82230d351ab6ae
Daniel Lohse <daniel.lenerd.lohse@gmail.com>
2016-10-12 14:20:10 +0200

    JourneyViewController added, including bottom layout

---
+++ Meemo/JourneyViewController.swift
@@ -0,0 +1,31 @@
+//
+//  JourneyViewController.swift
+//  Meemo
+//
+//  Created by Daniel Lohse on 10/12/16.
+//  Copyright © 2016 Superstudio. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class JourneyViewController: UIViewController{
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    override func viewDidLoad() {
+        scrollView.contentSize.height = 1000
+        let bottomOffset: CGPoint = CGPoint(x: 0, y: self.scrollView.contentSize.height);
+        scrollView.setContentOffset(bottomOffset, animated: false)
+        
+        
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+    }
+
+}
