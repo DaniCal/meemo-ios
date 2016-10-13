1de43fdc375e5eb752287b4b698de6e9fff56543
Daniel Lohse <daniel.lenerd.lohse@gmail.com>
2016-10-13 15:42:12 +0200

    ReactionViewController added including interaction methods

---
+++ Meemo/ReactionViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ReationViewController.swift
+//  Meemo
+//
+//  Created by Daniel Lohse on 10/13/16.
+//  Copyright © 2016 Superstudio. All rights reserved.
+//
+
+import UIKit
+
+class ReactionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func rockinDidTouch(_ sender: AnyObject) {
+    }
+    
+    @IBAction func downDidTouch(_ sender: AnyObject) {
+    }
+    
+    @IBAction func kickAssDidTouch(_ sender: AnyObject) {
+    }
+    
+    @IBAction func notSeriousDidTouch(_ sender: AnyObject) {
+    }
+    
+    @IBAction func brainwashDidTouch(_ sender: AnyObject) {
+    }
+}
