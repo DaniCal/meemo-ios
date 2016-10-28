c514f6c273db2163e5324f5687a15a9cfb3f3b54
Daniel Lohse <daniel.lenerd.lohse@gmail.com>
2016-10-28 15:01:55 +0200

    #20 new image asset added and replaced old one

--- Meemo/AppDelegate.swift
+++ Meemo/AppDelegate.swift
@@ -21,6 +21,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let launchedBefore = UserDefaults.standard.bool(forKey: "launchedBefore")
+        if(!launchedBefore){
+            
+        }
+
+        
+        
         return true
     }
 
