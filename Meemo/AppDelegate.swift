e25a7647f12c3a7cabf4ec36fc6e00c31925ccb5
Daniel Lohse <daniel.lenerd.lohse@gmail.com>
2016-10-28 17:51:58 +0200

    #1 FirebaseNotification through console added

--- Meemo/AppDelegate.swift
+++ Meemo/AppDelegate.swift
@@ -8,16 +8,21 @@
 
 import UIKit
 import Firebase
+import FirebaseInstanceID
+import FirebaseMessaging
+import UserNotifications
+
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate{
 
     var window: UIWindow?
 
 
     override init(){
-        FIRApp.configure()
         
     }
+    
+
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -34,10 +39,52 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             UserDefaults.standard.set(true, forKey: "launchedBefore")
 
         }
+        
+        connectToFCM()
+        
+        if #available(iOS 10.0, *) {
+            let authOptions : UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(
+                options: authOptions,
+                completionHandler: {_,_ in })
+            
+            
+            let settings: UIUserNotificationSettings = UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            application.registerUserNotificationSettings(settings)
+            
+            // For iOS 10 display notification (sent via APNS)
+            UNUserNotificationCenter.current().delegate = self
+            // For iOS 10 data message (sent via FCM)
+            FIRMessaging.messaging().remoteMessageDelegate = self
+            
+        } else {
+//            let settings: UIUserNotificationSettings =
+//                UIUserNotificationSettings(forTypes: [.alert, .badge, .sound], categories: nil)
+//            application.registerUserNotificationSettings(settings)
+        }
+        
+        application.registerForRemoteNotifications()
 
+        FIRApp.configure()
         
         
         return true
+    }
+    
+    func connectToFCM(){
+        FIRMessaging.messaging().connect{(error) in
+            if error != nil{
+                print("Unable to concect \(error)")
+            }else{
+                print("Connected to FCM")
+            }
+        }
+    }
+    
+    func tokenRefreshNotification(){
+        let refreshedToken = FIRInstanceID.instanceID().token()!
+        print("InstanceID token \(refreshedToken)")
+        connectToFCM()
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
@@ -61,7 +108,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
 
+@available(iOS 10, *)
+extension AppDelegate : UNUserNotificationCenterDelegate {
+    
+    // Receive displayed notifications for iOS 10 devices.
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        let userInfo = notification.request.content.userInfo
+        // Print message ID.
+        print("Message ID: \(userInfo["gcm.message_id"]!)")
+        
+        // Print full message.
+        print("%@", userInfo)
+    }
+}
+
+extension AppDelegate : FIRMessagingDelegate {
+    // Receive data message on iOS 10 devices.
+    func applicationReceivedRemoteMessage(_ remoteMessage: FIRMessagingRemoteMessage) {
+        print("%@", remoteMessage.appData)
+    }
+}
