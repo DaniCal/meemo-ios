6035db5f350bdbb81e871715556a564ed1540cf0
Daniel Lohse <daniel.lenerd.lohse@gmail.com>
2016-11-11 16:28:24 +0100

    ProgramTableViewCell added to scene & viewcontroller

--- Meemo/HomeViewController.swift
+++ Meemo/HomeViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var titles = ["First", "Second", "Third"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,14 +26,13 @@ class HomeViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "programCell", for: indexPath) as! ProgramTableViewCell
+        return cell
+    }
 
 }
---
+++ Meemo/ProgramTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  ProgramTableViewCell.swift
+//  Meemo
+//
+//  Created by Daniel Lohse on 11/11/16.
+//  Copyright © 2016 Superstudio. All rights reserved.
+//
+
+import UIKit
+
+class ProgramTableViewCell: UITableViewCell {
+
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
